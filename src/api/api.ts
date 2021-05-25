ac8142e1524bde27eb04ba479bc8815196aa5d5b
Andrey Prisniak <aprisniak@lectrum.io>
2021-05-25 13:15:19 +0300

    feat(source): add login and signup api methods

--- src/api/api.ts
+++ src/api/api.ts
@@ -31,4 +31,26 @@ export const api = {
 
         return newTip;
     },
+    async login(credentials) {
+        const { email, password } = credentials;
+        const { data } = await axios.get(
+            `${process.env.REACT_APP_API_URL}/auth/login`,
+            {
+                headers: {
+                    authorization: `Basic ${btoa(`${email}:${password}`)}`,
+                },
+            },
+        );
+
+        return data;
+    },
+    async signUp(credentials) {
+        const { confirmPassword, ...body } = credentials;
+        const { data } = await axios.post(
+            `${process.env.REACT_APP_API_URL}/auth/registration`,
+            body,
+        );
+
+        return data;
+    },
 };
