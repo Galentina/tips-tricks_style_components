61d5c82ee1e9e8da53133fad20df0fef4d7874e0
Galina Malareva <gala.malareva@gmail.com>
2021-05-28 21:00:16 +0200

    lesson-5 update

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -7,7 +7,13 @@ import { api } from '../api';
 export const useLogin = () => {
     const mutation = useMutation((credentials) => {
         return api.login(credentials);
+    },
+    {
+        onError(error) {
+            console.log('useLogin error', error);
+        },
     });
+
 
     return mutation;
 };
